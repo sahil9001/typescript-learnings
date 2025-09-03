d974f7b08814b32f7973a12352746e88a7af147d
Sahil Silare <sahilsilare@gmail.com>
2025-09-04 01:12:33 +0530

    feat: added `tsc` file
    Signed-off-by: Sahil Silare <sahilsilare@gmail.com>

---
+++ numbers-and-booleans.js
@@ -0,0 +1,10 @@
+// number
+var userId = 334455;
+// still a number
+var userIdDecimal = 334455.66;
+// smart enough to infer the type
+var userIdNew = 445522;
+// All the functions for the numbers will show up
+// userId.
+// another example
+var isLoggedIn = false;
