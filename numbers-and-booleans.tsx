18349def740664865ee7a6b7a28a56f67ef7ebbb
Sahil Silare <sahilsilare@gmail.com>
2025-09-04 01:12:07 +0530

    feat: added numbers and boolean file
    Signed-off-by: Sahil Silare <sahilsilare@gmail.com>

---
+++ numbers-and-booleans.tsx
@@ -0,0 +1,12 @@
+// number
+let userId: number = 334455
+// still a number
+let userIdDecimal: number = 334455.66
+// smart enough to infer the type
+let userIdNew = 445522;
+
+// All the functions for the numbers will show up
+// userId.
+
+// another example
+let isLoggedIn: boolean = false;
