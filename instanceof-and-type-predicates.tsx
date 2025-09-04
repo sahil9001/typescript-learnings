efd80e174b8e8bc926d0f262c6bb80b102a233d9
Sahil Silare <sahilsilare@gmail.com>
2025-09-04 23:47:23 +0530

    feat: added instanceof-and-type-predicates.tsx
    Signed-off-by: Sahil Silare <sahilsilare@gmail.com>

---
+++ instanceof-and-type-predicates.tsx
@@ -0,0 +1,10 @@
+// instanceof narrowing
+// JavaScript has an operator for checking whether or not a value is an “instance” of another value. More specifically, in JavaScript x instanceof Foo checks whether the prototype chain of x contains Foo.prototype. While we won’t dive deep here, and you’ll see more of this when we get into classes, they can still be useful for most values that can be constructed with new. As you might have guessed, instanceof is also a type guard, and TypeScript narrows in branches guarded by instanceofs.
+
+function logValue(x: Date | string) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    } else {
+        console.log(x.toUpperCase());
+    }
+}
