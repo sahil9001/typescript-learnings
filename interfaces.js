40498a223d60418d4bce6e628f75b0bd306cf566
Sahil Silare <sahilsilare@gmail.com>
2025-09-04 22:28:12 +0530

    feat: added interface code
    Signed-off-by: Sahil Silare <sahilsilare@gmail.com>

---
+++ interfaces.js
@@ -0,0 +1,7 @@
+"use strict";
+// very similar to types
+Object.defineProperty(exports, "__esModule", { value: true });
+var sahil = {
+    email: "sahil@s.com",
+    userId: 1234
+};
---
+++ interfaces.tsx
@@ -0,0 +1,14 @@
+// very similar to types
+
+// A contract for the implementer
+interface User {
+    email: string;
+    userId: number;
+}
+
+
+const sahil: User = {
+    email: "sahil@s.com",
+    userId: 1234
+}
+export { }
