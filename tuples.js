f2c2bb8680f5fae3373019807e3a62182a948287
Sahil Silare <sahilsilare@gmail.com>
2025-09-04 22:21:44 +0530

    feat: added `tuples.tsx`
    Signed-off-by: Sahil Silare <sahilsilare@gmail.com>

---
+++ tuples.js
@@ -0,0 +1,8 @@
+"use strict";
+// An array with some restrictions on it
+Object.defineProperty(exports, "__esModule", { value: true });
+var user = ['hc', 1];
+// Can be only added in specific format + order
+var tuser = ['hc', 1, true];
+// real world use case
+var rgb = [255, 123, 121];
---
+++ tuples.tsx
@@ -0,0 +1,11 @@
+// An array with some restrictions on it
+
+const user: (string | number)[] = ['hc', 1];
+
+// Can be only added in specific format + order
+const tuser: [string, number, boolean] = ['hc', 1, true];
+
+// real world use case
+let rgb: [number, number, number] = [255, 123, 121];
+
+export { }
