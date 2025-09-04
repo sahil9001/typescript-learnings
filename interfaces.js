10c132334980c301360c65407fbc47f213eb74b4
Sahil Silare <sahilsilare@gmail.com>
2025-09-04 22:31:27 +0530

    feat: added more on interfaces
    Signed-off-by: Sahil Silare <sahilsilare@gmail.com>

--- interfaces.js
+++ interfaces.js
@@ -2,6 +2,15 @@
 // very similar to types
 Object.defineProperty(exports, "__esModule", { value: true });
 var sahil = {
+    dbId: 123,
     email: "sahil@s.com",
-    userId: 1234
+    userId: 1234,
+    startTrail: function () {
+        return "trail started";
+    },
+    // Not taking any string and still works!!!
+    getCoupon: function () {
+        return 10;
+    },
+    githubToken: "abc",
 };
--- interfaces.tsx
+++ interfaces.tsx
@@ -2,13 +2,42 @@
 
 // A contract for the implementer
 interface User {
+    readonly dbId: number;
     email: string;
     userId: number;
+    googleId?: string;
+    // startTrail: () => string;
+    startTrail(): string;
+    getCoupon(couponname: string): number
 }
 
 
 const sahil: User = {
+    dbId: 123,
     email: "sahil@s.com",
-    userId: 1234
+    userId: 1234,
+    startTrail: () => {
+        return "trail started"
+    },
+    // Not taking any string and still works!!!
+    getCoupon: () => {
+        return 10
+    },
+    githubToken: "abc",
 }
+
+
+// Reopening a interface, existing + new property
+interface User {
+    githubToken: string
+}
+
+// Inheritance 
+interface Admin extends User {
+    role: "admin" | "ta" | "learner"
+}
+
+// Difference between type alias and interface
+// Interface can be extended while type cannot
+
 export { }
